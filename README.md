f606156aa78562ed83d14615233200d6b86e3ef2
Aaron Rembeck <aaron.rembeck@rdf.nuernberg.de>
2026-05-21 10:56:21 +0200

    V1 - neue Aufgabe, colab

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Some changes...
 
-New Username on GitHub.
+13.5.   New Username on GitHub.
+
+21.5.   [link] von Kollegen erhalten
+        " git clone [link] . " im Terminal eingeben (mit . wird Datei im aktuellen Ordner gespeichert)
+
+        main.py abgeändert nach Lehrer
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-print('Willkommen zu "Git-Demo".')
+# Funktion 1
+def strom_berechnen(leistung, spannung):
+    # TODO
 
-def hello():
-    print('Hello.')
+# Funktion 2
+def spannungsfall(strom, widerstand):
+    # Todo
 
-def bye():
-    print('Bye.')
+# Funktion 3
+def verlustleistung(strom, widerstand):
+    # TODO
+
+
+leistung = 2000   # Watt
+spannung = 230    # Volt
+widerstand = 0.5  # Ohm
+
+strom = strom_berechnen(leistung, spannung)
+u_verlust = spannungsfall(strom, widerstand)
+p_verlust = verlustleistung(strom, widerstand)
+
+print("Strom:", round(strom, 2), "A")
+print("Spannungsfall:", round(u_verlust, 2), "V")
+print("Verlustleistung:", round(p_verlust, 2), "W")
